e8385843e92aa856a80b41764d2b0c91ee8a57c1
fabiofonv <fabiofonv@hotmail.com>
2025-03-10 21:18:38 -0300

    cade_a_chave

--- app/controllers/ProjetosController.js
+++ app/controllers/ProjetosController.js
@@ -8,6 +8,13 @@ angular.module('meuApp')
             }
         }
 
+        $configContentUndefined = {
+            headers: {
+                'Authorization': 'Bearer ' + $token,
+                'Content-Type': undefined
+            }
+        }
+
         deslogar = function(){
             localStorage.removeItem('token');
             $state.go('login');
@@ -73,6 +80,7 @@ angular.module('meuApp')
             dataDeInicio: '',
             prioridade: 'normal',
             dataDeConclusao: '',
+            id_arquivo: '',
             tarefas: [],
             pontos: ''
         }
@@ -99,6 +107,14 @@ angular.module('meuApp')
             }
 
         }
+
+        $scope.setArquivo = function(input) {
+            var arquivo = input.files[0]; // Acessa o arquivo selecionado
+            if (arquivo) {
+                $scope.informacoes.arquivo = arquivo; // Armazena o arquivo no modelo
+                $scope.$apply(); // Força o AngularJS a atualizar o modelo
+            }
+        };
 
         $scope.adicionarTarefa = function () {
             $scope.novoProjeto.tarefas.push({ nome: '', id: Date.now() });
@@ -366,73 +382,79 @@ angular.module('meuApp')
 
         $scope.cadastrarNovoProjeto = function () {
             console.log($scope.novoProjeto);
+            var formData = new FormData();
+            formData.append('arquivo', $scope.informacoes.arquivo);
 
-
-
-
-            $http.post('http://localhost:8000/api/projetos/cadastrar', $scope.novoProjeto, $config).then(function (response) {
+            $http.post('http://localhost:8000/api/arquivos/salvar', formData, $configContentUndefined).then(function (response) {
                 console.log(response);
+                if(response.status == 201){
+                    $scope.novoUsuario.id_arquivo = response.data.id;
 
-                if (response.status == 201) {
+                    $http.post('http://localhost:8000/api/projetos/cadastrar', $scope.novoProjeto, $config).then(function (response) {
+                        console.log(response);
 
-
-                    for ($i = 0; $i < $scope.novoProjeto.tarefas.length; $i++) {
-
-                        // $tarefa->id_projeto = $request->id_projeto;
-                        // $tarefa->nome = $request->nome;
-                        post = {};
-                        post.nome = $scope.novoProjeto.tarefas[$i].nome;
-                        post.id_projeto = response.data.id;
+                        if (response.status == 201) {
 
 
-                        $http.post('http://localhost:8000/api/tarefas/cadastrar', post, $config).then(function (response) {
-                            console.log(response);
-                        }, function (error) {
-                            console.log(error);
-                        })
+                            for ($i = 0; $i < $scope.novoProjeto.tarefas.length; $i++) {
+
+                                // $tarefa->id_projeto = $request->id_projeto;
+                                // $tarefa->nome = $request->nome;
+                                post = {};
+                                post.nome = $scope.novoProjeto.tarefas[$i].nome;
+                                post.id_projeto = response.data.id;
+
+
+                                $http.post('http://localhost:8000/api/tarefas/cadastrar', post, $config).then(function (response) {
+                                    console.log(response);
+                                }, function (error) {
+                                    console.log(error);
+                                })
 
 
 
 
-                    }
+                            }
 
 
 
-                    $scope.limpar();
-                    Swal.fire({
-                        title: "Proposta cadastrada!",
-                        text: "Deseja cadastrar uma nova proposta?!",
-                        icon: "success",
-                        showCancelButton: true,
-                        confirmButtonColor: "#3085d6",
-                        cancelButtonColor: "#d33",
-                        confirmButtonText: "Sim, cadastrar nova!",
-                        cancelButtonText: "Não, eu acabei!"
-                    }).then((result) => {
-                        $scope.listar();
-                        if (result.isDismissed) {
+                            $scope.limpar();
+                            Swal.fire({
+                                title: "Proposta cadastrada!",
+                                text: "Deseja cadastrar uma nova proposta?!",
+                                icon: "success",
+                                showCancelButton: true,
+                                confirmButtonColor: "#3085d6",
+                                cancelButtonColor: "#d33",
+                                confirmButtonText: "Sim, cadastrar nova!",
+                                cancelButtonText: "Não, eu acabei!"
+                            }).then((result) => {
+                                $scope.listar();
+                                if (result.isDismissed) {
 
-                            $scope.$apply(function () {
-                                $scope.listandoProjetoAcao();
+                                    $scope.$apply(function () {
+                                        $scope.listandoProjetoAcao();
+                                    });
+
+
+                                }
                             });
-
-
                         }
+                    
+
+                }, function (error) {
+
+                    Swal.fire({
+                        title: "The Internet?",
+                        text: "That thing is still around?",
+                        icon: "question"
                     });
-                }
 
 
+                })
             }, function (error) {
-
-                Swal.fire({
-                    title: "The Internet?",
-                    text: "That thing is still around?",
-                    icon: "question"
-                });
-
-
+                console.log(error);
             })
-
 
         }
 
